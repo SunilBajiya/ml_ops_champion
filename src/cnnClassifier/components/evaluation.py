4d6a551253e492b868b66d76eb4ae711c5bfbe67
Sunil Kumar <sunil@algodox.co.in>
2025-08-28 17:19:38 +0530

    added the new code

--- src/cnnClassifier/components/evaluation.py
+++ src/cnnClassifier/components/evaluation.py
@@ -4,48 +4,52 @@ from src.cnnClassifier.entity.config_entity import EvaluationConfig
 from src.cnnClassifier.utils.common import save_json
 
 
+
 class Evaluation:
-    def __init__(self,config: EvaluationConfig):
+    def __init__(self, config: EvaluationConfig):
         self.config = config
-        
-        
+
+    
     def _valid_generator(self):
-        
-        datagenerator_kwargs= dict(
+
+        datagenerator_kwargs = dict(
             rescale = 1./255,
-            validation_split = 0.30
+            validation_split=0.30
         )
-        
+
         dataflow_kwargs = dict(
-            target_size= self.params_image_size[:-1],
+            target_size=self.config.params_image_size[:-1],
             batch_size=self.config.params_batch_size,
             interpolation="bilinear"
-            
         )
-        
-        valid_datagenerator= tf.keras.preprocessing.image.ImageDataGenerator(
+
+        valid_datagenerator = tf.keras.preprocessing.image.ImageDataGenerator(
             **datagenerator_kwargs
         )
-        self._valid_generator= valid_datagenerator.flow_from_directory(
-            directory= self.config.training_data,
+
+        self.valid_generator = valid_datagenerator.flow_from_directory(
+            directory=self.config.training_data,
             subset="validation",
             shuffle=False,
             **dataflow_kwargs
         )
-        
+
     
     @staticmethod
     def load_model(path: Path) -> tf.keras.Model:
         return tf.keras.models.load_model(path)
     
-    def evaluate(self):
-        model= self.load_model(self.config.path_of_model)
+
+    def evaluation(self):
+        model = self.load_model(self.config.path_of_model)
         self._valid_generator()
-        self.score = model.evaluate(self._valid_generator)
-           
-           
+        self.score = model.evaluate(self.valid_generator)
+
+    
     def save_score(self):
-        score = {"loss": self.score[0],
-                 "accuracy": self.score[1]
-                } 
-        save_json(path = Path("scores.json"), data= score)    
+        scores = {"loss": self.score[0], "accuracy": self.score[1]}
+        save_json(path=Path("scores.json"), data=scores)
+
+    
+
+    
